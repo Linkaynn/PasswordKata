58db4cfd23fb80144d3195d781c54fc566b566a2
Jesé Romero Arbelo <jese@synergiart.com>
2016-02-07 00:22:38 +0000

    Password should have at least one uppercase character.

--- src/password_checker/Password.java
+++ src/password_checker/Password.java
@@ -2,6 +2,10 @@ package password_checker;
 
 public class Password {
     public boolean isValid(String password) {
-        return password.length() >= 6;
+        return password.length() >= 6 && hasUpperCase(password);
+    }
+
+    private boolean hasUpperCase(String password) {
+        return password.chars().mapToObj(value -> (char) value).anyMatch(character -> (int) character >=65 && (int) character <= 90);
     }
 }
--- test/password_checker/_Password.java
+++ test/password_checker/_Password.java
@@ -9,7 +9,13 @@ import static org.junit.Assert.assertThat;
 public class _Password {
     @Test
     public void password_should_have_at_least_6_chars_long() throws Exception {
-        assertThat(new Password().isValid("asdfgh"), is(true));
+        assertThat(new Password().isValid("asdfGh"), is(true));
         assertThat(new Password().isValid("asdfg"), is(false));
+    }
+
+    @Test
+    public void password_should_have_at_least_one_uppercase_character() throws Exception {
+        assertThat(new Password().isValid("aAdfgh"), is(true));
+        assertThat(new Password().isValid("asddgd"), is(false));
     }
 }
